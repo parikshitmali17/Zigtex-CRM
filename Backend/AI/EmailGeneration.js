88c3fb1c23800d8deea71c82f5021c02ef422667
Parikshit <Parikshitmali17@gmail.com>
2025-07-30 12:19:54 +0530

    Gemini integrated standatd Routes

---
+++ Backend/AI/EmailGeneration.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+export const router = Router();
+
+router.post('/ai/EmailGeneration', async (req, res) => {
+    console.log(req.body);
+    const userInput = req.body.message;
+    const ai = new GoogleGenAI({ apiKey: dotenv.config().parsed.Gemeni_API_KEY });
+    
+    try {
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: userInput,
+        });
+        console.log(response.text);
+        res.json({ reply: response.text });
+    } catch (error) {
+        console.error("Error generating content:", error);
+        res.status(500).json({ error: "Failed to generate content" });
+    }
+})
--- Backend/Server.js
+++ Backend/Server.js
@@ -1,25 +1,16 @@
 // const express = require('express');
 import express from "express"
+import { router as AIEmailgenerationRouter} from "./AI/EmailGeneration.js";
+
 const app = express();
 
 app.use(express.json());
-
-import { GoogleGenAI } from "@google/genai";
-import dotenv from "dotenv";
+app.use('/', AIEmailgenerationRouter);
 
 
-app.post('/', async (req, res) => {
-    console.log(req.body);
-    const userInput = req.body.message; ;
-    const ai = new GoogleGenAI({apiKey:dotenv.config().parsed.Gemeni_API_KEY});
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: userInput,
-  });
-  console.log(response.text);
-  res.json({ reply: response.text });
 
-})
+
+
 
 
 app.listen(3000,()=>{
